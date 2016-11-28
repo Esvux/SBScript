23572d811f8acd5f446caab4f3d88c19affecf58
esvux <esvux@esvux-Aspire-V5-471>
2016-11-28 00:44:51 -0600

    Commit inicial

---
+++ src/org/esvux/sbscript/ventanas/Inicio.java
@@ -0,0 +1,135 @@
+package org.esvux.sbscript.ventanas;
+
+import javax.swing.text.Document;
+import javax.swing.text.PlainDocument;
+
+/**
+ *
+ * @author esvux
+ */
+public class Inicio extends javax.swing.JFrame {
+
+    public Inicio() {
+        initComponents();
+        Document doc = jTextPane_Programa.getDocument();
+        doc.putProperty(PlainDocument.tabSizeAttribute, 4);
+        jTextPane_Programa.setDocument(doc);
+        TextLineNumber tln = new TextLineNumber(jTextPane_Programa);
+        jScrollPane_Programa.setRowHeaderView(tln);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jSplitPane_Principal = new javax.swing.JSplitPane();
+        jScrollPane_Programa = new javax.swing.JScrollPane();
+        jTextPane_Programa = new javax.swing.JTextPane();
+        jTabbedPane1 = new javax.swing.JTabbedPane();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jSplitPane_Principal.setDividerLocation(500);
+
+        jTextPane_Programa.setFont(new java.awt.Font("Andale Mono", 0, 14)); // NOI18N
+        jTextPane_Programa.setText("Principal(){\n\tMostrar(\"Hola\", \"Mundo\");\n}");
+        jTextPane_Programa.setCaretPosition(0);
+        jScrollPane_Programa.setViewportView(jTextPane_Programa);
+
+        jSplitPane_Principal.setLeftComponent(jScrollPane_Programa);
+
+        jTabbedPane1.setTabPlacement(javax.swing.JTabbedPane.BOTTOM);
+
+        jTextArea1.setEditable(false);
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jTabbedPane1.addTab("   Salida   ", jScrollPane1);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null}
+            },
+            new String [] {
+                "Descripción", "Lin : Col"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                true, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jTable1.getTableHeader().setReorderingAllowed(false);
+        jScrollPane2.setViewportView(jTable1);
+
+        jTabbedPane1.addTab("   Errores   ", jScrollPane2);
+
+        jSplitPane_Principal.setRightComponent(jTabbedPane1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jSplitPane_Principal, javax.swing.GroupLayout.DEFAULT_SIZE, 872, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jSplitPane_Principal, javax.swing.GroupLayout.DEFAULT_SIZE, 447, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("GTK+".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Inicio().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane_Programa;
+    private javax.swing.JSplitPane jSplitPane_Principal;
+    private javax.swing.JTabbedPane jTabbedPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextPane jTextPane_Programa;
+    // End of variables declaration//GEN-END:variables
+}
